4084c8fc34b1e6424125e90531e729d3bfafd506
diego polidori <d.poldo@libero.it>
2022-10-11 17:16:05 +0200

    Corrected a typo

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main() {
 	editor.SetAudioBuffer(audioFile.samples, resize_factor);
 	editor.SaveAudioFile(path_to_new_file);
 	/****************************/
-	//This dous not compile....
+	//This does not compile....
 	//AudioEditorGeneric<double> genEditor;
 	//genEditor.SetAudioFileParams(audioFile);
 
